55bc9ef72a99c3a56bf83a80fd55b57c090044fc
esethuraman <elattp@gmail.com>
2018-10-02 14:34:12 -0400

    tests refactored

--- unitTestPractise/src/test/java/com/esethuraman/learning/unitTests/unitTestPractise/BusinessTestsWithMock.java
+++ unitTestPractise/src/test/java/com/esethuraman/learning/unitTests/unitTestPractise/BusinessTestsWithMock.java
@@ -3,44 +3,37 @@ package com.esethuraman.learning.unitTests.unitTestPractise;
 import com.esethuraman.learning.unitTests.business.BusinessImpl;
 import com.esethuraman.learning.unitTests.business.DataService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class BusinessTestsWithMock {
 
+    BusinessImpl business;
+    DataService mockedDataService;
+    @Before
+    public void setUp(){
+        business = new BusinessImpl();
+        mockedDataService = Mockito.mock(DataService.class);
+        business.setDataService(mockedDataService);
+    }
+
     @Test
     public void calculateSum_usingMockito(){
-        BusinessImpl business = new BusinessImpl();
-        DataService mockedDataService = Mockito.mock(DataService.class);
+
         Mockito.when(mockedDataService.retrieveIntArray()).thenReturn(new int[]{1,2,3,4,5});
-
-        int actual = business.calculateSum(mockedDataService.retrieveIntArray());
-        int expected = 15;
-
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(business.calculateSumFromDataService(), 15);
     }
 
     @Test
-    public void calculateSum_oneElementArrayusingMockito(){
-        BusinessImpl business = new BusinessImpl();
-        DataService mockedDataService = Mockito.mock(DataService.class);
+    public void calculateSum_oneElementArrayUsingMockito(){
         Mockito.when(mockedDataService.retrieveIntArray()).thenReturn(new int[]{2});
-
-        int actual = business.calculateSum(mockedDataService.retrieveIntArray());
-        int expected = 2;
-
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(business.calculateSumFromDataService(), 2);
     }
 
     @Test
-    public void calculateSum_emptyArrayusingMockito(){
-        BusinessImpl business = new BusinessImpl();
-        DataService mockedDataService = Mockito.mock(DataService.class);
+    public void calculateSum_emptyArrayUsingMockito(){
         Mockito.when(mockedDataService.retrieveIntArray()).thenReturn(new int[]{});
-
-        int actual = business.calculateSum(mockedDataService.retrieveIntArray());
-        int expected = 0;
-
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(business.calculateSumFromDataService(), 0);
     }
 }
